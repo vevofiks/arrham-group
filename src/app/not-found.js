5d4973cae0faccf7440b8dbe27d7955b1bb8a58c
mhd asjad <mhdasjad877@gmail.com>
2025-09-06 18:29:17 +0530

    fix:css error

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -7,16 +7,16 @@ const montserrat = MontserratFont({
 });
 export default function NotFound() {
   return (
-    <div className=" rounded-4xl h-[90vh] mt-28  bg-gradient-to-b from-red-700 px-2 mx-2 to-black">
-    {/* <div className=" rounded-4xl h-[90vh] mt-28 bg-gradient-to-b from-lgreen px-2 to-black"> */}
+    <div className=" rounded-4xl h-[90vh] mt-28  bg-gradient-to-b from-red-800 px-2 mx-2 to-black">
+      {/* <div className=" rounded-4xl h-[90vh] mt-28 bg-gradient-to-b from-lgreen px-2 to-black"> */}
       <div className=" pt-32 overflow-x-hidden">
-      
-      <ScrollVelocity
-  texts={['Page Not Found', '404 Error']} 
-  velocity={50} 
-  className={`custom-scroll-text ${montserrat.className} text-white text-6xl md:text-8xl`}
-/>
-      
+
+        <ScrollVelocity
+          texts={['Page Not Found', '404 Error']}
+          velocity={50}
+          className={`custom-scroll-text ${montserrat.className} text-white text-6xl md:text-8xl`}
+        />
+
       </div>
 
     </div>
